890a9020979e1c644181228032415f50801cbb3d
saad7528 <saad0174742@gmail.com>
2026-05-27 13:33:40 +0600

    add actions.js

---
+++ src/app/lib/actions.js
@@ -0,0 +1,10 @@
+const deleteUser = async (userId) => {
+    'use server'
+
+    const res = await fetch(`http://localhost:5000/users/${userId}`, {
+        method: 'DELETE'
+    });
+    const data = await res.json();
+    return data;
+
+}
--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -4,31 +4,31 @@ export const getUsers = async () =>{
     return data;
 }
 
-// export const getUserById = async (userId) => {
-//     const res = await fetch(`http://localhost:5000/users/${userId}`);
-//     const data = await res.json();
-//     return data;
-// }   
-
 export const getUserById = async (userId) => {
-    console.log("User Id", userId);
-    try {
-        // cache: 'no-store' যুক্ত করা হলো, যাতে Next.js পুরনো ক্যাশ করা HTML এরর না দেখায়
-        const res = await fetch(`http://localhost:5000/users/${userId}`, {
-            cache: 'no-store' 
-        });
+    const res = await fetch(`http://localhost:5000/users/${userId}`);
+    const data = await res.json();
+    return data;
+}   
 
-        // রেসপন্স ঠিক না থাকলে (যেমন 500 বা 404 হলে) জোর করে JSON পার্স করবে না
-        if (!res.ok) {
-            console.error(`Backend returned an error! Status: ${res.status}`);
-            return null; // অথবা আপনার প্রয়োজন অনুযায়ী error handle করুন
-        }
+// export const getUserById = async (userId) => {
+//     console.log("User Id", userId);
+//     try {
+//         // cache: 'no-store' যুক্ত করা হলো, যাতে Next.js পুরনো ক্যাশ করা HTML এরর না দেখায়
+//         const res = await fetch(`http://localhost:5000/users/${userId}`, {
+//             cache: 'no-store' 
+//         });
 
-        const data = await res.json();
-        return data;
+//         // রেসপন্স ঠিক না থাকলে (যেমন 500 বা 404 হলে) জোর করে JSON পার্স করবে না
+//         if (!res.ok) {
+//             console.error(`Backend returned an error! Status: ${res.status}`);
+//             return null; // অথবা আপনার প্রয়োজন অনুযায়ী error handle করুন
+//         }
 
-    } catch (error) {
-        console.error("Failed to fetch user:", error);
-        return null;
-    }
-}
+//         const data = await res.json();
+//         return data;
+
+//     } catch (error) {
+//         console.error("Failed to fetch user:", error);
+//         return null;
+//     }
+// }
